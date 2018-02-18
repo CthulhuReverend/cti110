c6f1bb93627f012935d407d0142fad3975f55004
CthulhuReverend <36601975+CthulhuReverend@users.noreply.github.com>
2018-02-18 10:13:48 -0500

    Add files via upload
    First Upload

---
+++ P2T1_GloverAustin.py
@@ -0,0 +1,10 @@
+#CTI-110
+#P2T1 - Sales Prediction
+#Austin Glover
+#18-Feb-18
+
+#Annual Total Sales = 0.23
+
+totalSales = float(input("Enter Projected Sales:"))
+annualProfit = float(totalSales*0.23)
+print("This is your annual profit: $", format(annualProfit, ",.2f"))
